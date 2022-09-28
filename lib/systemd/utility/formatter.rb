4e97fb0e33fe6d4edbf4b92f0dbd2bdfb1adc3e4
magic4dev <magic4dev@gmail.com>
2022-09-28 19:38:38 +0200

    added system error exception to return_an_array_from_system_command method of Systemd::Utility::Formatter class

--- lib/systemd/utility/formatter.rb
+++ lib/systemd/utility/formatter.rb
@@ -3,7 +3,12 @@ module Systemd
         class Formatter
             # method for convert `` system call to array based list
             def self.return_an_array_from_system_command(system_call)
-                system_call.chomp!.split(/\n/)
+                # remove \n from system call returned value
+                system_call_without_new_line = system_call.chomp!
+                # return system error message 
+                return(system_call_without_new_line) if !$?.success?
+                # convert `` system call to array based list
+                system_call_without_new_line.split(/\n/)
             end
         end
     end
