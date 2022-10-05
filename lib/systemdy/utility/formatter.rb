955426bb4452bbe203fe2309c4f82d3f1ea0d47e
magic4dev <magic4dev@gmail.com>
2022-10-05 18:48:55 +0200

    fix yard params description in Systemdy::Utility::Formatter class

--- lib/systemdy/utility/formatter.rb
+++ lib/systemdy/utility/formatter.rb
@@ -3,17 +3,17 @@ module Systemdy
     module Utility 
         # Allows to formatting provided data into another type
         class Formatter
-            # a method for convert system calls into an array 
+            # method for convert system calls into an array 
             #
-            # @param  [String] a system call to convert to an array
-            # @return [Array]  an array based list of a system call execution
+            # @param  system_call [String] system call to convert to an array
+            # @return [Array] an array-based list of the values ​​returned by making a system call
             # @example convert a backtick system call into an array
             #   list_of_files_in_my_folder = Systemdy::Utility::Formatter.return_an_array_from_system_command(`ls -la`)
-            # @todo execute system calls with backtick instead of system() beacuse system() return only true or false and not the data that you expect
+            # @todo execute system calls with backtick instead of system() beacuse system() return only true or false and not the expected value
             def self.return_an_array_from_system_command(system_call)
                 system_call_without_new_line = system_call.chomp!    # remove \n from system call returned value
-                return(system_call_without_new_line) if !$?.success? # return system error message if the process has non-zero exit status
-                system_call_without_new_line.split(/\n/)             # convert `` system call to array based list
+                return system_call_without_new_line if !$?.success?  # return system error message if the process has non-zero exit status
+                system_call_without_new_line.split(/\n/)             # convert values returned by `` system call to an array-based list
             end
         end
     end
