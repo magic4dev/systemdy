84a60819a5c00c95bf4c17812182d18415ac5c41
magic4dev <magic4dev@gmail.com>
2022-10-05 20:03:47 +0200

    added yard-style comments for the check_if_e_service_exist method of the Systemdy::Utility::Validator class

--- lib/systemdy/utility/validator.rb
+++ lib/systemdy/utility/validator.rb
@@ -1,12 +1,20 @@
 module Systemdy
+    # A module that contains a set of useful classes for add utilities to the Systemdy's core
     module Utility 
+        # Allows to add validatation to a class
         class Validator
-            # method for check if a service exist
-            def self.check_if_a_service_exist(service)
-                `#{SYSTEMCTL_COMMAND} list-unit-files #{service}.service | wc -l`.to_i > 3
+            # a method for check if a service is intalled on the system
+            #
+            # @param name_of_the_service [String] the name of the service
+            # @return [Boolean] the presence of the service on the system
+            # @example check if a service is intalled on the system
+            #   postgresql_service = Systemdy::Utility::Validator.check_if_a_service_exist('postgresql')
+            #   #=> true
+            def self.check_if_a_service_exist(name_of_the_service)
+                `#{SYSTEMCTL_COMMAND} list-unit-files #{name_of_the_service}.service | wc -l`.to_i > 3
                 # 'systemctl list-unit-files name_of_service.service | wc -l' command output:
-                #  3  when there are not matching units
-                #  >3 when there are matching units
+                #  3  when there are not matching units(the service not exist because isn't installed on the system)
+                #  >3 when there are matching units(the service exist because is installed on the system)
             end
         end
     end
