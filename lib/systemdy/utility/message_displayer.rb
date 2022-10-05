aed34b2ee2fb55074dc6b4155cec9cbc7c44b160
magic4dev <magic4dev@gmail.com>
2022-10-05 19:27:22 +0200

    added yard-style comments for the display_message  method of the Systemdy::Utility::MessageDisplayer class

--- lib/systemdy/utility/message_displayer.rb
+++ lib/systemdy/utility/message_displayer.rb
@@ -1,9 +1,17 @@
 module Systemdy
+    # A module that contains a set of useful classes for add utilities to the Systemdy's core
     module Utility 
+        # Allows to display messages to the user
         class MessageDisplayer
-            # method for render custom message tho the user
+            # a method for render custom message to the user
+            #
+            # @param message [String] the message to render 
+            # @return [String] the content of the message
+            # @example a custom message to show to the user
+            #   custom_message = Systemdy::Utility::MessageDisplayer.render_message('my custom message')
+            #   #=> "my custom message"
             def self.render_message(message)
-                message
+                message # the content of the message
             end
         end
     end
