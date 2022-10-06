868f188bd2295bd2f26b7ffae5b76e2a9b55316f
magic4dev <magic4dev@gmail.com>
2022-10-06 19:18:20 +0200

    changed comments in Systemdy::Service with a yard-like syntax for generate documentation

--- lib/systemdy/service.rb
+++ lib/systemdy/service.rb
@@ -1,6 +1,9 @@
 module Systemdy
+    # Allows to control a life-cycle of a systemd's service
+    # @attr_reader command [String] the default 'systemctl' command
+    # @attr_reader name [String] the name of the service
     class Service 
-
+            
         # extend Forwardable standard's library module for delegate a specified method to a designated object
         extend Forwardable
 
@@ -18,45 +21,84 @@ module Systemdy
 
         attr_reader :command, :name
 
-        # we create a new object that accept 1 argument:
-        # 1. the name of the Systemdy service to control (postgresql, redis etc..)
-        # Example:
-        # my_postgresql_service = Systemdy::Service.new('postgresql')
+        # method for create a new Systemdy::Service object 
+        #
+        # @param  name [String] the name of the systemd service to control
+        # @return [Systemdy::Service] a new Systemdy::Service object
+        # @example Create an object
+        #    my_postgresql_service = Systemdy::Service.new('postgresql')
         def initialize(name)
             @command   = SYSTEMCTL_COMMAND # constant contained in Systemdy.rb
             @name      = name 
         end
 
-        # we delegate return_an_array_from_system_command method to Systemdy::Utility::Formatter class contained in Systemdy/utility/formatter.rb
+        # delegate return_an_array_from_system_command method to Systemdy::Utility::Formatter class contained in Systemdy/utility/formatter.rb
         def_delegator Systemdy::Utility::Formatter,        :return_an_array_from_system_command
-        # we delegate render_message method to Systemdy::Utility::MessageDisplayer class contained in Systemdy/utility/message_displayer.rb
+        # delegate render_message method to Systemdy::Utility::MessageDisplayer class contained in Systemdy/utility/message_displayer.rb
         def_delegator Systemdy::Utility::MessageDisplayer, :render_message 
-        # we delegate check_if_a_service_exist method to Systemdy::Utility::Validator class contained in Systemdy/utility/validator.rb
+        # delegate check_if_a_service_exist method to Systemdy::Utility::Validator class contained in Systemdy/utility/validator.rb
         def_delegator Systemdy::Utility::Validator,        :check_if_a_service_exist 
-        # we delegate filter_by_keys method to Systemdy::Utility::KeyValueFilter class contained in Systemdy/utility/key_value.rb
+        # delegate filter_by_keys method to Systemdy::Utility::KeyValueFilter class contained in Systemdy/utility/key_value.rb
         def_delegator Systemdy::Utility::KeyValueFilter,   :filter_by_keys 
 
-        # method for check if a created service exist
-        # Example:
-        # my_postgresql_service.exist?
-        # if the provided service exist this method return
-        # - true
-        # otherwise return
-        # - false
+        # method for check if a created service +exist+
+        #
+        # @return [Boolean] the presence of the service on the system
+        # @example Create an object
+        #   my_postgresql_service.exist? #=> true
         def exist? 
             check_if_a_service_exist(name) # class method contained in Systemdy/utility/validator.rb
         end
         
         # create dynamically methods based on LIST_OF_ACTIONS constant
-        # after created a new object we can call the methods:
-        # my_postgresql_service.start
-        # my_postgresql_service.restart
-        # my_postgresql_service.stop
-        # my_postgresql_service.enable
-        # my_postgresql_service.disable
-        # my_postgresql_service.reload
-        # my_postgresql_service.mask
-        # my_postgresql_service.unmask
+        # @!method start
+        #   execute action +start+ on the service
+        #   @example start a service
+        #       my_postgresql_service.start
+        #   @note This method is generated with use of metaprogramming techniques
+        #
+        # @!method restart
+        #   execute action +restart+ on the service
+        #   @example restart a service
+        #       my_postgresql_service.restart
+        #   @note This method is generated with use of metaprogramming techniques
+        #
+        # @!method stop
+        #   execute action +stop+ on the service
+        #   @example stop a service
+        #       my_postgresql_service.stop
+        #   @note This method is generated with use of metaprogramming techniques
+        #
+        # @!method enable
+        #   execute action +anable+ on the service
+        #   @example enable a service
+        #       my_postgresql_service.enable
+        #   @note This method is generated with use of metaprogramming techniques
+        #
+        # @!method disable
+        #   execute action +disable+ on the service
+        #   @example disable a service
+        #       my_postgresql_service.disable
+        #   @note This method is generated with use of metaprogramming techniques
+        #
+        # @!method reload
+        #   execute action +reload+ on the service
+        #   @example reload a service
+        #       my_postgresql_service.reload
+        #   @note This method is generated with use of metaprogramming techniques
+        #
+        # @!method mask
+        #   execute action +mask+ on the service
+        #   @example mask a service
+        #       my_postgresql_service.mask
+        #   @note This method is generated with use of metaprogramming techniques
+        #
+        # @!method unmask
+        #   execute action +unmask+ on the service
+        #   @example unmask a service
+        #       my_postgresql_service.unmask
+        #   @note This method is generated with use of metaprogramming techniques
+        #
         LIST_OF_ACTIONS.each do |action|
             define_method action do 
                 sudo = Etc.getpwuid(Process.uid).name != 'root' ? 'sudo' : ''
@@ -64,54 +106,74 @@ module Systemdy
             end
         end
 
-        # method for return a key/value pair of the provided service's properties
-        # Example:
-        # my_postgresql_service.properties
-        # return a key/value pair of the provided service's properties
-        # { "Type"=>"oneshot",
-        #   "Restart"=>"no",
-        #   "NotifyAccess"=>"none",
-        #   ....
-        # }
+        # method for return a key/value pair of the service's properties
+        #
+        # @return [Hash] the service's properties
+        # @example display all the properties related to a service
+        #   
+        #   my_postgresql_service.properties 
+        #
+        #     {
+        #       "Type"=>"oneshot",              
+        #       "Restart"=>"no",          
+        #       "ExecMainPID"=>"48615",
+        #       "NotifyAccess"=>"none"
+        #     }
+        #
+        # @note For the sake of brevity, all service-related properties are not shown in this example
         def properties
             array_of_properties = return_an_array_from_system_command(`#{command} show #{name}`)
             array_of_properties.collect { |property| { property.split('=')[0] => property.split('=')[1] } }.reduce({}, :merge)
         end
 
         # method for return the current status of the provided service
-        # Example:
-        # my_postgresql_service.status
-        # return a key/value pair of the provided service's status
-        # { "Id"=>"postgresql.service",              
-        #   "Description"=>"PostgreSQL RDBMS",          
-        #   "ExecMainPID"=>"48615",
-        #   "LoadState"=>"loaded",
-        #   "ActiveState"=>"active",
-        #   "FragmentPath"=>"/lib/Systemdy/system/postgresql.service",
-        #   "ActiveEnterTimestamp"=>"Thu 2022-09-29 17:13:07 CEST",
-        #   "InactiveEnterTimestamp"=>"Thu 2022-09-29 17:12:44 CEST",
-        #   "ActiveExitTimestamp"=>"Thu 2022-09-29 17:12:44 CEST",
-        #   "InactiveExitTimestamp"=>"Thu 2022-09-29 17:13:07 CEST"
-        # } 
+        #
+        # @return [Hash] the service's current status
+        # @example display the current status of the service
+        #   my_postgresql_service.status
+        #
+        #     {
+        #       "Id"=>"postgresql.service",              
+        #       "Description"=>"PostgreSQL RDBMS",          
+        #       "ExecMainPID"=>"48615",
+        #       "LoadState"=>"loaded",
+        #       "ActiveState"=>"active",
+        #       "FragmentPath"=>"/lib/Systemdy/system/postgresql.service",
+        #       "ActiveEnterTimestamp"=>"Thu 2022-09-29 17:13:07 CEST",
+        #       "InactiveEnterTimestamp"=>"Thu 2022-09-29 17:12:44 CEST",
+        #       "ActiveExitTimestamp"=>"Thu 2022-09-29 17:12:44 CEST",
+        #       "InactiveExitTimestamp"=>"Thu 2022-09-29 17:13:07 CEST"
+        #     }
+        #
         def status 
             exist? ? filter_by_keys(properties, LIST_OF_STATUS_PROPERTIES) : default_error_message()
         end
 
         # create dynamically methods based on LIST_OF_STATUSES constant
-        # after created a new object we can call the methods:
-        # my_postgresql_service.is_enabled?
-        # my_postgresql_service.is_active?
-        # if the provided service is active or enabled this method return
-        # - true
-        # otherwise return
-        # - false
+        # @!method is_enabled?
+        #   check if the service is +enabled+
+        #   @return [Boolean] the service is enabled
+        #   @example check if a service is enabled
+        #       my_postgresql_service.is_enabled? #=> true
+        # @note This method is generated with use of metaprogramming techniques
+        #
+        # @!method is_active?
+        #   check if the service is +active+
+        #   @return [Boolean] the service is active
+        #   @example check if a service is active
+        #       my_postgresql_service.is_active? #=> true
+        # @note This method is generated with use of metaprogramming techniques
         LIST_OF_STATUSES.each do |status|
             define_method "is_#{status}?" do 
                 exist? ? return_an_array_from_system_command(`#{command} is-#{status} #{name}`).include?(status) : default_error_message()
             end
         end
 
-        # method for display error when a service or unit not exist
+        # method to show a message when an action is performed on a service not installed on the system
+        # @return [String] the error message
+        # @example the action start on a service not installed on the system
+        #     a_service_not_installed.start #=> "Unit a_service_not_installed.service could not be found."
+        # @!visibility private
         def default_error_message
             render_message("Unit #{name}.service could not be found.") # class method contained in Systemdy/utility/message_displayer.rb
         end
