4c82b228dd566a4d56fc16a519d9d58204b47761
magic4dev <magic4dev@gmail.com>
2022-10-07 19:16:52 +0200

    added yard-like comments for documentation of the class

--- lib/systemdy/journal.rb
+++ lib/systemdy/journal.rb
@@ -1,29 +1,20 @@
 module Systemdy
+    # Allows to filter journalctl logs
     class Journal 
 
         # extend SingleForwardableForwardable standard's library module for delegate a specified method to a designated object
         extend SingleForwardable
                
         # list of options for execute journalctl command that not accept arguments
-        # Example command: journalctl -k
-        # the '-k' option not accept arguments
-        # journalctl -k [argument] return
-        # Failed to add match [argument]: Invalid argument
+        # @note The meaning of this constant is that in linux the command ``` journalctl -k ``` not accept arguments
         LIST_OF_OPTIONS_THAT_NOT_ACCEPT_ARGUMENTS = { kernel: '-k' }
         
         # list of options for execute journalctl command that accept arguments
-        # Example command: journalctl -b
-        # the '-b' option accept an argument (the number of the boot)
-        # journalctl -b postgresql return the boots's logs
-        # journalctl -b 3 return the third of the availables boot logs
+        # @note The meaning of this constant is that in linux the command ``` journalctl -b ``` can accept arguments, if executed without argument return the last boot logs
         LIST_OF_OPTIONS_THAT_ACCEPT_AN_ARGUMENT   = { boot: '-b' }
 
         # list of options for execute journalctl command that require arguments
-        # Example command: journalctl -u
-        # the '-u' option require an argument (the name of the unit)
-        # journalctl -u postgresql return the postgresql's logs
-        # journalctl -u without an argument return
-        # journalctl: option requires an argument -- 'u' 
+        # @note The meaning of this constant is that in linux the command like ``` journalctl -u ``` require arguments, if executed without argument return an error
         LIST_OF_OPTIONS_THAT_REQUIRE_AN_ARGUMENT  = { unit: '-u', group_id: '_GID', user_id: '_UID' }
 
         # we delegate return_an_array_from_system_command method to Systemdy::Utility::Formatter class contained in Systemdy/utility/formatter.rb
@@ -31,19 +22,24 @@ module Systemdy
         # we delegate render_message method to Systemdy::Utility::MessageDisplayer class contained in Systemdy/utility/message_displayer.rb
         def_delegator Systemdy::Utility::MessageDisplayer, :render_message 
 
-        # create dynamically class methods based on LIST_OF_OPTIONS_THAT_NOT_ACCEPT_ARGUMENTS constant
-        # we can call the methods:
-        # Systemdy::Journal.display_kernel_logs
-        # this method accept 3 keyword arguments:
-        # 1. since ('the log's initial period')  - String  - default 'yesterday' 
-        # 2. to    ('the log's end period')      - String  - default Time.now.strftime('%H:%M')
-        # 3. lines ('the log's number of lines') - Integer - default 10 .display_kernel_logs
-        # Example:
-        # Systemdy::Journal.display_kernel_logs(since: '1 month ago', lines: 50)
-        # if you call this method without arguments
-        # - it return an array with 10 lines of logs
-        LIST_OF_OPTIONS_THAT_NOT_ACCEPT_ARGUMENTS.each do |message_from, option|
-            define_singleton_method "display_#{message_from}_logs" do |since: 'today', to: Time.now.strftime('%H:%M'), lines: 10|
+        # create dynamically methods based on LIST_OF_OPTIONS_THAT_NOT_ACCEPT_ARGUMENTS constant
+        # @!scope class
+        # @!method display_kernel_logs
+        #   display the +kernel logs+
+        #   @param  since [String] the log's initial period - **default**: +today+
+        #   @param  to [String] the log's end period - **default**: +timenow+
+        #   @param  lines [Integer] the log's number of lines - **default**: +10+
+        #   @return [Array] a list of kernel logs
+        #   @example display the last 10 lines of kernel logs since today to the time execution method
+        #       Systemdy::Journal.display_kernel_logs #=> [...]
+        #   @example display the last the last 20 log lines ranging from a week ago to yesterday
+        #       Systemdy::Journal.display_kernel_logs(since: '1 week ago', to: 'yesterday', lines: 20) #=> [...]
+        #   @example You can also filter the logs by specific dates in the format 'YYYY-MM-DD'
+        #       Systemdy::Journal.display_kernel_logs(since: '2022-08-27', lines: 200) #=> [...]
+        #   @note This method is generated with use of metaprogramming techniques
+        #
+        LIST_OF_OPTIONS_THAT_NOT_ACCEPT_ARGUMENTS.each do |from, option|
+            define_singleton_method "display_#{from}_logs" do |since: 'today', to: Time.now.strftime('%H:%M'), lines: 10|
                 # logs from system call
                 logs = `#{JOURNALCTL_COMMAND} #{option} -S '#{since}' -U '#{to}' -n #{lines} | tail -n #{lines} 2>&1`
                 # logs from system call converted into array
@@ -51,20 +47,27 @@ module Systemdy
             end
         end
         
-        # create dynamically class methods based on LIST_OF_OPTIONS_THAT_ACCEPT_AN_ARGUMENT constant
-        # we can call the methods:
-        # Systemdy::Journal.display_boot_logs
-        # this method accept 4 keyword arguments:
-        # 1. argument ('the boot's number')      - Integer - optional
-        # 2. since ('the log's initial period')  - String  - default 'yesterday' 
-        # 3. to    ('the log's end period')      - String  - default Time.now.strftime('%H:%M')
-        # 4. lines ('the log's number of lines') - Integer - default 10 
-        # Example:
-        # Systemdy::Journal.display_boot_logs(argument: 3, since: '1 month ago', lines: 50)
-        # if you call this method without arguments
-        # - it return an array with 10 lines of logs
-        LIST_OF_OPTIONS_THAT_ACCEPT_AN_ARGUMENT.each do |message_from, option|
-            define_singleton_method "display_#{message_from}_logs" do |argument: '', since: 'today', to: Time.now.strftime('%H:%M'), lines: 10|
+        # create dynamically methods based on LIST_OF_OPTIONS_THAT_ACCEPT_AN_ARGUMENT constant
+        # @!scope class
+        # @!method display_boot_logs
+        #   display the +boot logs+
+        #   @param  argument [Integer] the boot number
+        #   @param  since [String] the log's initial period - **default**: +today+
+        #   @param  to [String] the log's end period - **default**: +timenow+
+        #   @param  lines [Integer] the log's number of lines - **default**: +10+
+        #   @return [Array] a list of boot logs
+        #   @example display the last 10 lines of kernel logs since today to the time execution method
+        #       Systemdy::Journal.display_boot_logs #=> [...]
+        #   @example display the last 50 log lines of the second boot ranging from a month ago to 10:00 of the current day
+        #       Systemdy::Journal.display_boot_logs(argument: 2, since: '1 month ago', to: '10:00', lines: 50) #=> [...]
+        #   @example display the last 20 log of the third boot lines ranging from a week ago to yesterday
+        #       Systemdy::Journal.display_boot_logs(argument: 3, since: '1 week ago', to: 'yesterday', lines: 20) #=> [...]
+        #   @example You can also filter the logs by specific dates in the format 'YYYY-MM-DD'
+        #       Systemdy::Journal.display_boot_logs(since: '2022-08-27', lines: 200) #=> [...]
+        #   @note This method is generated with use of metaprogramming techniques
+        #
+        LIST_OF_OPTIONS_THAT_ACCEPT_AN_ARGUMENT.each do |from, option|
+            define_singleton_method "display_#{from}_logs" do |argument: '', since: 'today', to: Time.now.strftime('%H:%M'), lines: 10|
                 # logs from system call
                 logs = `#{JOURNALCTL_COMMAND} #{option} #{argument} -S '#{since}' -U '#{to}' -n #{lines} | tail -n #{lines} 2>&1`
                 # logs from system call converted into array
@@ -73,27 +76,63 @@ module Systemdy
         end
         
         # create dynamically class methods based on LIST_OF_OPTIONS_THAT_REQUIRE_AN_ARGUMENT constant
-        # we can call the methods:
-        # Systemdy::Journal.display_unit_logs
-        # Systemdy::Journal.display_group_id_logs
-        # Systemdy::Journal.display_user_id_logs
-        # this method accept 4 keyword arguments:
-        # 1. argument ('the unit, the group_id or user_id) - String  - required
-        # 2. since ('the log's initial period')  - String  - default 'yesterday' 
-        # 3. to    ('the log's end period')      - String  - default Time.now.strftime('%H:%M')
-        # 4. lines ('the log's number of lines') - Integer - default 10 
-        # Example:
-        # Systemdy::Journal.display_unit_logs(argument: 'postgresql', since: '1 month ago', lines: 50)
-        # Systemdy::Journal.display_user_id_logs(argument: 1000, since: '1 month ago', lines: 50)
-        # Systemdy::Journal.display_group_id_logs(argument: 1000, since: '1 month ago', lines: 50)
-        LIST_OF_OPTIONS_THAT_REQUIRE_AN_ARGUMENT.each do |message_from, option|
-            define_singleton_method "display_#{message_from}_logs" do |argument: '', since: 'today', to: Time.now.strftime('%H:%M'), lines: 10|
+        # @!scope class
+        # @!method display_unit_logs
+        #   display the +unit logs+
+        #   @param  argument [string] the unit name
+        #   @param  since [String] the log's initial period - **default**: +today+
+        #   @param  to [String] the log's end period - **default**: +timenow+
+        #   @param  lines [Integer] the log's number of lines - **default**: +10+
+        #   @return [Array] a list of unit logs
+        #   @example display the last 50 log lines of the potsgresql service ranging from a month ago to 10:00 of the current day
+        #       Systemdy::Journal.display_unit_logs(argument: 'postgresql', since: '1 month ago', to: '10:00', lines: 50) #=> [...]
+        #   @example display the last 20 log lines of the potsgresql service ranging from a week ago to yesterday
+        #       Systemdy::Journal.display_unit_logs(argument: 'postgresql', since: '1 week ago', to: 'yesterday', lines: 20) #=> [...]
+        #   @example You can also filter the logs by specific dates in the format 'YYYY-MM-DD'
+        #       Systemdy::Journal.display_unit_logs(argument: 'postgresql', since: '2022-08-27', lines: 200) #=> [...]
+        #   @note This method is generated with use of metaprogramming techniques
+        #   @todo This method require the unit name as argument. For more information check out the examples below
+        #
+        # @!method display_group_id_logs
+        #   display the +group_id logs (GUID)+
+        #   @param  argument [Integer] the group id
+        #   @param  since [String] the log's initial period - **default**: +today+
+        #   @param  to [String] the log's end period - **default**: +timenow+
+        #   @param  lines [Integer] the log's number of lines - **default**: +10+
+        #   @return [Array] a list of group id logs
+        #   @example display the last 50 log lines of the GUID 1000 ranging from a month ago to 10:00 of the current day
+        #       Systemdy::Journal.display_group_id_logs(argument: 1000, since: '1 month ago', to: '10:00', lines: 50) #=> [...]
+        #   @example display the last 20 log lines of the GUID 1000 ranging from a week ago to yesterday
+        #       Systemdy::Journal.display_group_id_logs(argument: 1000, since: '1 week ago', to: 'yesterday', lines: 20) #=> [...]
+        #   @example You can also filter the logs by specific dates in the format 'YYYY-MM-DD'
+        #       Systemdy::Journal.display_group_id_logs(argument: 1000, since: '2022-08-27', lines: 200) #=> [...]
+        #   @note This method is generated with use of metaprogramming techniques
+        #   @todo This method require the GUID as argument. For more information check out the examples below
+        #
+        # @!method display_user_id_logs
+        #   display the +user_id logs (UID)+
+        #   @param  argument [Integer] the user id
+        #   @param  since [String] the log's initial period - **default**: +today+
+        #   @param  to [String] the log's end period - **default**: +timenow+
+        #   @param  lines [Integer] the log's number of lines - **default**: +10+
+        #   @return [Array] a list of user id logs
+        #   @example display the last 50 log lines of the UID 1000 ranging from a month ago to 10:00 of the current day
+        #       Systemdy::Journal.display_user_id_logs(argument: 1000, since: '1 month ago', to: '10:00', lines: 50) #=> [...]
+        #   @example display the last 20 log lines of the UID 1000 ranging from a week ago to yesterday
+        #       Systemdy::Journal.display_user_id_logs(argument: 1000, since: '1 week ago', to: 'yesterday', lines: 20) #=> [...]
+        #   @example You can also filter the logs by specific dates in the format 'YYYY-MM-DD'
+        #       Systemdy::Journal.display_user_id_logs(argument: 1000, since: '2022-08-27', lines: 200) #=> [...]
+        #   @note This method is generated with use of metaprogramming techniques
+        #   @todo This method require the UID as argument. For more information check out the examples below
+        #
+        LIST_OF_OPTIONS_THAT_REQUIRE_AN_ARGUMENT.each do |from, option|
+            define_singleton_method "display_#{from}_logs" do |argument: '', since: 'today', to: Time.now.strftime('%H:%M'), lines: 10|
                 # return an error message if the required argument is not provided
                 # render_message class method contained in Systemdy/utility/message_displayer.rb
-                return render_message("display_#{message_from}_logs require an argument!") if argument.to_s.empty?
+                return render_message("display_#{from}_logs require an argument!") if argument.to_s.empty?
                 # combination of option and argument based on typology
                 # '-u postgresql' or '_GUID=1000' or '_UID=1000'
-                option_with_argument = merge_option_with_argument_based_on_option_typology(message_from, option, argument)
+                option_with_argument = merge_option_with_argument_based_on_option_typology(from, option, argument)
                 # logs from system call
                 logs                 = `#{JOURNALCTL_COMMAND} #{option_with_argument} -S '#{since}' -U '#{to}' | tail -n #{lines} 2>&1`
                 # logs from system call converted into array
@@ -101,12 +140,31 @@ module Systemdy
             end
         end
 
+        # @!scope class
+        # @!method return_an_array_from_system_command
+        #   @param system_call [String] system call to convert to an array
+        #   @return [Array] an array-based list of the values ​​returned by making a system call
+        #   @note check out more about this method in Systemdy/utility/formatter.rb
+        #
+        # @!scope class
+        # @!method render_message
+        #   @param message [String] the message to render 
+        #   @return [String] the content of the message
+        #   @note check out more about this method in Systemdy/utility/message_displayer.rb
+        #
+
         # method for return formatted option
-        # Example
-        # merge_option_with_argument_based_on_option_typology(:unit, '-u', 'postgresql') return '-u postgresql'
-        # merge_option_with_argument_based_on_option_typology(:group_id, '_GUID', 1234566) return '_GUID=1000'
-        def self.merge_option_with_argument_based_on_option_typology(message_from, option, argument)
-            message_from == :unit ? "#{option} #{argument}" : "#{option}=#{argument}"
+        #
+        # @param  type [String] the type of command to bind
+        # @param  option [String] the option 
+        # @param  argument [String] the argument
+        # @return [String] the formatted option
+        # @example return formatted option
+        #   merge_option_with_argument_based_on_option_typology(:unit, '-u', 'postgresql') #=> '-u postgresql'
+        #   merge_option_with_argument_based_on_option_typology(:group_id, '_GUID', 1000) #=> '_GUID=1000'
+        #   merge_option_with_argument_based_on_option_typology(:user_id, '_UID', 1000) #=> '_UID=1000'
+        def self.merge_option_with_argument_based_on_option_typology(type, option, argument)
+            type == :unit ? "#{option} #{argument}" : "#{option}=#{argument}"
         end
 
         # make the methods below as private
